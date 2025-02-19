2a2e72fcca195645af3993324765e7b3dd7a000e
Lisa Carvalho <i.m.lisaaa14@gmail.com>
2025-02-19 23:43:03 +0000

    Ex2

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -1,3 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Bye world");
+﻿namespace MyProject;
+
+/// <summary>
+/// Is this a tag?
+/// </summary>
+class Program
+{
+    /// <summary>
+    /// I dont get it
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Bye world");
+    }
+}

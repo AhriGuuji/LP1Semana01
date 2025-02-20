4f1b16c9b192a6a8236e300042044c38075762b9
Lisa Carvalho <i.m.lisaaa14@gmail.com>
2025-02-20 15:55:10 +0000

    Add inteiros

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            int number = 8;
+            int another = 9;
+            int inteiro = 5;
+            Console.WriteLine($"{number}\n{another}\n{inteiro}");
+
+        }
+    }
+}

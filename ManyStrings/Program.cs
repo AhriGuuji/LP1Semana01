a7ec1d264fb9e1b988ac3041418deb9275e83632
Lisa Carvalho <i.m.lisaaa14@gmail.com>
2025-02-20 16:41:01 +0000

    Add strings

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ManyStrings
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string hello = "Hello LP1!";
+            string aspas = "Existe algo \"Entre\" aspas";
+            string heart = "Coração é assim ❤";
+            Console.WriteLine($"{hello}\n{heart}\n{aspas}");
+        }
+    }
+}

e99f804c5c70e87b13e1be23c12459553f065655
Lisa Carvalho <i.m.lisaaa14@gmail.com>
2025-02-20 17:47:02 +0000

    Add Finish

--- AskForInput/Program.cs
+++ AskForInput/Program.cs
@@ -10,10 +10,12 @@ namespace AskForInput
             string pedirNumero = Console.ReadLine();
             int number = int.Parse(pedirNumero);
             Console.WriteLine(number);
+
             Console.WriteLine("Insira um número real:");
             string pedirDecimal = Console.ReadLine();
             float aPartOfSomething = float.Parse(pedirDecimal);
             Console.WriteLine(aPartOfSomething);
+            
             Console.WriteLine($"A soma dos dois números é {number+aPartOfSomething}");
         }
     }

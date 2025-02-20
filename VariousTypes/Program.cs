6be81a1dbf23e4990f54b66bea6b6e7c03605e4c
Lisa Carvalho <i.m.lisaaa14@gmail.com>
2025-02-20 16:03:23 +0000

    Add symbols

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -10,6 +10,10 @@ namespace VariousTypes
             int another = 9;
             int inteiro = 5;
             Console.WriteLine($"{number}\n{another}\n{inteiro}");
+            char music = '♫';
+            char pencil = '✎';
+            char mail = '✉';
+            Console.WriteLine($"{music}\n{pencil}\n{mail}");
 
         }
     }

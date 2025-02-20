de50d7691e1111ee22ac208c154a3691fef3b2b0
Lisa Carvalho <i.m.lisaaa14@gmail.com>
2025-02-20 17:22:38 +0000

    Add conversões

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -6,6 +6,8 @@ namespace ManyStrings
     {
         private static void Main(string[] args)
         {
+            double xx = 1.23456;
+            int ii = 19;
             string hello = "Hello LP1!";
             string aspas = "Existe algo \"Entre\" aspas";
             string heart = "Coração é assim ❤";
@@ -17,6 +19,14 @@ namespace ManyStrings
             int y = 3;
             Console.WriteLine($"O valor de x e' {x}");
             Console.WriteLine($"{x} mais {y} e' igual a {x+y}");
+            string xx1 = $"{xx:f2}";
+            string xx2 = $"{xx:f1}";
+            string ii1 = $"{ii:x}";
+            string ii2 = $"{xx:c}";
+            Console.WriteLine(xx1);
+            Console.WriteLine(xx2);
+            Console.WriteLine(ii1);
+            Console.WriteLine(ii2);
         }
     }
 }

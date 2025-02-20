1952625d270c55b6ede507a2eb21a4a9f481d734
Lisa Carvalho <i.m.lisaaa14@gmail.com>
2025-02-20 16:56:19 +0000

    Add concatenação e interpolação

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -10,6 +10,13 @@ namespace ManyStrings
             string aspas = "Existe algo \"Entre\" aspas";
             string heart = "Coração é assim ❤";
             Console.WriteLine($"{hello}\n{heart}\n{aspas}");
+            string somaDeChars = "a" + 2;
+            int x = 2;
+            string somaDeVariaveis = "abc" + x;
+            Console.WriteLine($"{somaDeChars}\n{somaDeVariaveis}");
+            int y = 3;
+            Console.WriteLine($"O valor de x e' {x}");
+            Console.WriteLine($"{x} mais {y} e' igual a {x+y}");
         }
     }
 }

be438b76ced57caef12e2948af3e488da9b0698b
Lisa Carvalho <i.m.lisaaa14@gmail.com>
2025-02-20 15:28:18 +0000

    Add tags

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -2,8 +2,15 @@
 
 namespace MeuPrograma
 {
+    /// <summary>
+    /// Is this a tag?
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// is about it.
+        /// </summary>
+        /// <param name="args"></param>
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP1!");

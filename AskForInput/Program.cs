acb079b2ae0df224ad64fc1aad1e94316fc1d881
Lisa Carvalho <i.m.lisaaa14@gmail.com>
2025-02-20 17:40:46 +0000

    Add for reals

--- AskForInput/Program.cs
+++ AskForInput/Program.cs
@@ -6,9 +6,15 @@ namespace AskForInput
     {
         private static void Main(string[] args)
         {
+            Console.WriteLine("Insira um número inteiro:");
             string pedirNumero = Console.ReadLine();
             int number = int.Parse(pedirNumero);
             Console.WriteLine(number);
+            Console.WriteLine("Insira um número real:");
+            string pedirDecimal = Console.ReadLine();
+            float aPartOfSomething = float.Parse(pedirDecimal);
+            Console.WriteLine(aPartOfSomething);
+
         }
     }
 }

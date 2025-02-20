df512cac8b5e03d917869671a97f2728ef0bebdb
Lisa Carvalho <i.m.lisaaa14@gmail.com>
2025-02-20 17:31:14 +0000

    Add input

---
+++ AskForInput/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AskForInput
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string pedirNumero = Console.ReadLine();
+            Console.WriteLine(pedirNumero);
+        }
+    }
+}
